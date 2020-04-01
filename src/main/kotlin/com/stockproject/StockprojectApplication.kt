f886a17e379a66c7b8d6f5b8a12799212c1fd261
Andreas Wassum <a.wassum@gmail.com>
2020-04-01 18:51:17 +0200

    Initial commit

---
+++ src/main/kotlin/com/stockproject/StockprojectApplication.kt
@@ -0,0 +1,11 @@
+package com.stockproject
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class StockprojectApplication
+
+fun main(args: Array<String>) {
+    runApplication<StockprojectApplication>(*args)
+}
